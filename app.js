fa3de2570239e29ad7a5aa893948147a10f15d75
jessi <166540018+JessicaKauane017@users.noreply.github.com>
2024-11-18 14:23:27 -0300

    js

---
+++ app.js
@@ -0,0 +1,5 @@
+function criaCartão(categoria,pergunta,responta) {
+    let container = document.getElementById('container')
+}   let cartao = document.createElement('article')
+    let cartao = document.createElement('article')
+    
